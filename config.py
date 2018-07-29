b95f5c47cc36c3c83c22b26401f86363a8bc0e98
Kishore Ramakrishnan <38735766+kishore145@users.noreply.github.com>
2018-07-29 16:45:20 -0700

    Added working code and saved plots

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "WLfW8sXBHdV0KFgQ2cQ3xQDHr"
+consumer_secret = "wGlChXIoHLAlGxmBQLaa9SVDi4HRj4kxCggTlndJ2eYT8ue1RL"
+access_token = "1019763113254502400-kjUEjoE5q7up3kKFvN56Gsmoiifa3h"
+access_token_secret = "jImF6JcUkYfbSkb7C74vlHCF9CQO35q3TVroqQ3VVdzre"
